b589c1dc87dfe041bb7f3593f02ab50e44dbb21c
Parth-06 <parth99.thakkar@gmail.com>
2022-12-08 23:37:33 +0530

    first commit

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require("express");
 const app = express();
 
 let cors = require("cors");
-
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://celadon-kringle-01bd97.netlify.app",
+  })
+);
 dotenv.config({ path: "./config.env" });
 require("./DB/conn");
 
+app.use("/uploads", express.static("/uploads"));
 app.use(express.json());
 
 app.use(require("./router/auth"));
@@ -15,12 +21,7 @@ app.use(require("./router/PostfetchAuth"));
 app.use(require("./router/ProfileAuth"));
 app.use(require("./router/UserProfileAuth.js"));
 app.use(require("./router/FollowUnFollowAuth.js"));
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://filtersocial.netlify.app",
-  })
-);
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`listning At ${PORT}`);
