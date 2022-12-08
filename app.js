06eacd87a2a7ac933cc4c32236c5ddca1af7a0a8
Parth-06 <parth99.thakkar@gmail.com>
2022-12-08 22:32:56 +0530

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 
-const cors = require("cors");
+let cors = require("cors");
 
 dotenv.config({ path: "./config.env" });
 require("./DB/conn");
@@ -11,27 +11,19 @@ app.use("/uploads", express.static("/uploads"));
 app.use(express.json());
 app.use(cors());
 
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://celadon-kringle-01bd97.netlify.app",
+  })
+);
+
 app.use(require("./router/auth"));
 app.use(require("./router/LoginRegiAuth"));
 app.use(require("./router/PostfetchAuth"));
 app.use(require("./router/ProfileAuth"));
 app.use(require("./router/UserProfileAuth.js"));
 app.use(require("./router/FollowUnFollowAuth.js"));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  if ("OPTIONS" == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
